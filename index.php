cb59191bf265ad0edd923d39f7010f207485214a
Cauã Thurler <cauathurler.ysy@gmail.com>
2026-01-14 17:51:55 -0300

    commit final de hoje

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 require_once __DIR__ . '/pages/index_controller.php';
 require_once __DIR__ . '/pages/index_view.php';
+require_once __DIR__ . '/helpers/bootstrap_admin.php';
+bootstrap_initial_admin($pdo);
